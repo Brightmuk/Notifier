8bd21b004c1ddfd2dbfe3069bed1ec0e59fdfa01
Brightmuk <be.rightmuk@gmail.com>
2019-09-27 21:24:12 +0300

    Create the connect and disconnect function for socket

--- notifier/consumers.py
+++ notifier/consumers.py
@@ -1,11 +1,17 @@
 import asyncio
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-class TickTockConsumer(AsyncJsonWebsocketConsumer):
+class NoseyConsumer(AsyncJsonWebsocketConsumer):
+    
     async def connect(self):
         await self.accept()
-        while 1:
-            await asyncio.sleep(1)
-            await self.send_json('tick')
-            await asyncio.sleep(1)
-            await self.send_json('.....tick')
+        await self.channel_layer.group_add('gossip',self.channel_name)
+        print(f'Added {self.channel_name} to gossip')
+
+    async def disconnect(self):
+        await self.channel_layer.group_discard('gossip',self.channel_name)
+        print(f'Discarded {self.channel_name} from gossip')
+
+    async def user_gossip(self, event):
+        await self.send_json(event)
+        print(f'Got message {event} at {self.channel_name}')
