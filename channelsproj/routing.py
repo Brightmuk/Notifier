0f433081c9f8cadc3ba99b0927b8b2f8ceccf1ed
Brightmuk <be.rightmuk@gmail.com>
2019-09-27 12:43:11 +0300

    Creation of the initial application base

---
+++ channelsproj/routing.py
@@ -0,0 +1,10 @@
+from channels.routing import ProtocolTypeRouter,URLRouter
+from django.urls import path
+
+from notifier.consumers import TickTockConsumer
+
+application = ProtocolTypeRouter({
+    'websocket':URLRouter([
+        path('ws/',TickTockConsumer),
+    ])
+})
