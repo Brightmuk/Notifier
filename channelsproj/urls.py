4a8c40ed59f399dd540ecd77f7a6c50e0ad5ea93
Brightmuk <be.rightmuk@gmail.com>
2019-09-28 13:32:30 +0300

    Create default home page

--- channelsproj/urls.py
+++ channelsproj/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from notifier.views import HomeView
+from notifier.views import *
 
 urlpatterns = [
     path('',HomeView.as_view()),
+    path('notification/',NotificationView.as_view()),
     path('admin/', admin.site.urls),
 
 ]
--- notifier/views.py
+++ notifier/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class NotificationView(TemplateView):
+    template_name = 'other.html'
